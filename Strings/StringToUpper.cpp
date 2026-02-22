009c6a2dcbd677b7b3417f5b345adc9f127b5735
Sidharth P Pai <sidharthppai@gmail.com>
2026-02-22 22:36:24 +0530

    Create StringToUpper.cpp

---
+++ Strings/StringToUpper.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int strlength(char* source) {
+
+	int count = 0;
+
+	while(*source != 0){
+		count++;
+		source++;
+	}
+	return count;	
+}
+
+void toUpper(char* source) {
+
+	
+	int strlen = strlength(source);
+
+	for (int i = 0; i < strlen; i++) {
+	
+		if (*source>='a' && *source <= 'z'){
+
+
+			printf("%c\t , %d\n",*source,*sourceS);
+			*source -= 32;
+
+		}
+		source++;	
+
+	}
+}
+
+
+int main(int argc,char *argv[]) {
+	if(argc < 2) {
+		printf("Command line error \n");
+		return -1;
+	}
+
+	toUpper(argv[1]);
+	printf("The Strings is  : %s\n ",argv[1]);
+
+	return 0;
+}
